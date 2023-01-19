6a519cd40e2ac2fe9b471a71d7bf786d9d747e37
tmsksfh2012 <tmsksfh2012@naver.com>
2023-01-19 12:58:42 +0900

    23/01/19 다이어리2 뷰 제작 완료

--- recordOfMemory/app/src/main/java/com/recordOfMemory/src/main/home/HomeFragment.kt
+++ recordOfMemory/app/src/main/java/com/recordOfMemory/src/main/home/HomeFragment.kt
@@ -2,11 +2,48 @@ package com.recordOfMemory.src.main.home
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.FragmentTransaction
 import com.recordOfMemory.R
+import com.recordOfMemory.config.ApplicationClass.Companion.sSharedPreferences
 import com.recordOfMemory.config.BaseFragment
 import com.recordOfMemory.databinding.FragmentDiary2Binding
+import com.recordOfMemory.src.main.home.diary2.recycler.Diary2RecyclerVIewAdapter
 
 class HomeFragment : BaseFragment<FragmentDiary2Binding>(FragmentDiary2Binding::bind, R.layout.fragment_diary2_empty){
+
+    lateinit var itemList : ArrayList<ItemData>
+    inner class itemListAdapterToList {
+        fun getItemId(item: ItemData) {
+            openItem(item)
+        }
+    }
+
+    fun openItem(item: ItemData) {
+        val itemFragment = ItemFragment(item)
+
+        val fm = requireActivity().supportFragmentManager
+        val transaction: FragmentTransaction = fm.beginTransaction()
+
+        val email = sSharedPreferences.getString("email", "")
+        if(email == "NO") {
+            transaction
+                .replace(R.id.main_frm, LoginFragment())
+                .addToBackStack(null)
+                .commit()
+            transaction.isAddToBackStackAllowed
+        }
+        else {
+            transaction
+                .replace(R.id.main_frm, itemFragment)
+                .addToBackStack(null)
+                .commit()
+            transaction.isAddToBackStackAllowed
+        }
+    }
+
+    lateinit var diary2RecyclerVIewAdapter : Diary2RecyclerVIewAdapter
+
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -15,6 +52,18 @@ class HomeFragment : BaseFragment<FragmentDiary2Binding>(FragmentDiary2Binding::
         }
         binding.diary2IvGrid.setOnCheckedChangeListener { buttonView, isChecked ->
             binding.diary2IvList.isChecked = !isChecked
+        }
+
+        val items = itemListAdapterToList()
+
+        val homeLayoutManager = GridLayoutManager(context, 2)
+
+        homeRecyclerViewAdapter = HomeRecyclerViewAdapter(items, itemList)
+
+        binding.homeRecycler.apply {
+            layoutManager = homeLayoutManager
+            adapter = homeRecyclerViewAdapter
+            homeRecyclerViewAdapter.filter.filter("wood")
         }
 
 //        binding.homeButtonTryGetJwt.setOnClickListener {
