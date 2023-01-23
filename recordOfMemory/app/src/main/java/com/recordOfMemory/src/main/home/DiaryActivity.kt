4912bf4000bda236195a4bb18890293689d1551e
jiwoorld <bncmdbev@gmail.com>
2023-01-23 16:34:03 +0900

    2023/01/23 - Diary View 수정

--- recordOfMemory/app/src/main/java/com/recordOfMemory/src/main/home/DiaryActivity.kt
+++ recordOfMemory/app/src/main/java/com/recordOfMemory/src/main/home/DiaryActivity.kt
@@ -9,15 +9,18 @@ import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.recordOfMemory.R
+import com.recordOfMemory.databinding.ActivityDiaryBinding
 import com.recordOfMemory.src.main.home.Diary.DiaryAdapter
 import com.recordOfMemory.src.main.home.Diary.DiaryData
 
 class DiaryActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDiaryBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_diary)
-
-        val diary_rv = findViewById<RecyclerView>(R.id.diary_rv)
+        binding = ActivityDiaryBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
         val itemList = ArrayList<DiaryData>()
 
@@ -34,37 +37,21 @@ class DiaryActivity : AppCompatActivity() {
         val DiaryAdapter = DiaryAdapter(itemList)
         DiaryAdapter.notifyDataSetChanged()
 
-        /*diary_rv.adapter =  DiaryAdapter
-        diary_rv.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)*/
-
-        diary_rv.adapter = DiaryAdapter
+        binding.diaryRv.adapter = DiaryAdapter
         val gridLayoutManager = GridLayoutManager(this, 3, GridLayoutManager.VERTICAL, false)
-        diary_rv.setLayoutManager(gridLayoutManager)
+        binding.diaryRv.setLayoutManager(gridLayoutManager)
 
-        var btn_alone: Button = findViewById<Button>(R.id.diary_btn_alone)
-        var btn_together: Button = findViewById<Button>(R.id.diary_btn_together)
-        var btn_add: Button = findViewById<Button>(R.id.icon_diary_add)
-
-
-        btn_alone.setSelected(true);
-        btn_alone.setOnClickListener {
-            btn_alone.setSelected(true);
-            btn_together.setSelected(false);
+        binding.diaryBtnAlone.setSelected(true);
+        binding.diaryBtnAlone.setOnClickListener {
+            binding.diaryBtnAlone.setSelected(true);
+            binding.diaryBtnTogether.setSelected(false);
         }
-        btn_together.setOnClickListener {
-            btn_together.setSelected(true);
-            btn_alone.setSelected(false);
+        binding.diaryBtnTogether.setOnClickListener {
+            binding.diaryBtnTogether.setSelected(true);
+            binding.diaryBtnAlone.setSelected(false);
         }
-        /*btn_add.setOnClickListener() {
-            Toast
-                .makeText(this, "toast message", Toast.LENGTH_SHORT)
-                .show()
-        }*/
 
-        /*var binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)*/
-
-        btn_add.setOnClickListener {
+        binding.iconDiaryAdd.setOnClickListener {
             // Dialog만들기
             val mDialogView = LayoutInflater.from(this).inflate(R.layout.fragment_pop_diary, null)
             val mBuilder = AlertDialog.Builder(this)
@@ -80,8 +67,6 @@ class DiaryActivity : AppCompatActivity() {
                     .makeText(this, "일기_전체보기로 이동", Toast.LENGTH_SHORT)
                     .show()
             }
-
-
         }
     }
 }
